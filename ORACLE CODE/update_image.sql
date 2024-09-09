e222d6b5b6467e0f254a46ca525674b120992dac
Samira akter <64495945+samiraakter98@users.noreply.github.com>
2024-09-10 03:02:29 +0600

    Add files via upload

---
+++ ORACLE CODE/update_image.sql
@@ -0,0 +1,15 @@
+UPDATE DRIVER
+SET IMAGE='static/images/driver_image/15.png'
+WHERE DRIVER_ID = 13
+UPDATE DRIVER
+SET IMAGE='static/images/driver_image/14.png'
+WHERE DRIVER_ID = 14
+UPDATE DRIVER
+SET IMAGE='static/images/driver_image/15.jpg'
+WHERE DRIVER_ID = 15
+UPDATE DRIVER
+SET IMAGE='static/images/driver_image/NO_IMAGE.png'
+WHERE DRIVER_ID = 16
+UPDATE DRIVER
+SET IMAGE='static/images/driver_image/NO_IMAGE.png'
+WHERE DRIVER_ID = 17
